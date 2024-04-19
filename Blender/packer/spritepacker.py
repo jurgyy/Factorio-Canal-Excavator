ba2b9234766ca7724e8f8fafd9c402f69aad5c03
Jurgen <jurgen.baas@outlook.com>
2024-04-19 09:40:42 +0200

    Added options to give an achor image as offset such that if another image has to be shifted beyond the pack's calculated shift, the packer can give the matching shift for a second sprite sheet.

--- Blender/packer/spritepacker.py
+++ Blender/packer/spritepacker.py
@@ -3,6 +3,33 @@ import os
 from pathlib import Path
 from PIL import Image
 
+
+class AnchorShift:
+    def __init__(self, anchorXOffset, anchorYOffset, anchorXShift, anchorYShift):
+        if anchorXOffset is None \
+        or anchorYOffset is None \
+        or anchorXShift is None \
+        or anchorYShift is None:
+            raise Exception("All offset and shift arguments have to have a value")
+        
+        self.AnchorXOffset = anchorXOffset
+        self.AnchorYOffset = anchorYOffset
+        self.AnchorXShift = anchorXShift
+        self.AnchorYShift = anchorYShift
+
+    def CalcAnchorOffset(self, scale, xOffset, yOffset):
+        def calc(givenOffset, fixedOffset, secondGivenOffset):
+            scaleOffset = givenOffset * scale
+            totalOffset = fixedOffset - scaleOffset
+            return secondGivenOffset * scale + totalOffset
+        
+        if scale is None:
+            scale = 1
+
+        return calc(self.AnchorXOffset, self.AnchorXShift, xOffset), \
+               calc(self.AnchorYOffset, self.AnchorYShift, yOffset)
+
+
 _upper_bound = 99999
 
 
@@ -15,7 +42,14 @@ def iter_image(files, directory: Path, extension: str, sort=False):
         yield Image.open(fpath)
 
 
-def pack(directory: Path, output: Path, extension: str, scale=None, threshold=None):
+def offset(scale, fixedOffset, givenOffset, secondGivenOffset):
+  scaleOffset = givenOffset * scale
+  totalOffset = fixedOffset - scaleOffset
+  return secondGivenOffset * scale + totalOffset
+
+
+def pack(directory: Path, output: Path, extension: str, scale=None, threshold: float = None,
+         anchorShift: AnchorShift = None):
     print(directory)
     print(output)
 
@@ -100,11 +134,16 @@ def pack(directory: Path, output: Path, extension: str, scale=None, threshold=No
     shift_x = (orig_width - min_left - max_right) / -2
     shift_y = (orig_height - min_upper - max_lower) / -2
 
-    print("\t-- Include shift if you want to keep centering of the original image:")
-    if scale is not None:
-        print(f"\tshift = util.by_pixel({shift_x * scale}, {shift_y * scale}),")
+    if anchorShift is not None:
+        print(f"\t-- Shift before offset: {shift_x}, {shift_y}")
+        shift_x, shift_y = anchorShift.CalcAnchorOffset(scale, shift_x, shift_y)
+    elif scale is not None:
+        shift_x = shift_x * scale
+        shift_y = shift_y * scale
     else:
-        print(f"\tshift = util.by_pixel({shift_x}, {shift_y}), -- Multiply these two values by your animation scale")
+        print("\t-- Multiply these two values by your animation scale:")
+    print("\t-- Include shift if you want to keep centering of the original image:")
+    print(f"\tshift = util.by_pixel({shift_x}, {shift_y}), ")
 
     print(f"\tframe_count = {n}, -- Depends on your frame_sequence")
 
@@ -118,6 +157,12 @@ def parse_arguments():
     parser.add_argument("--threshold", "-t", help="Alpha channel threshold $alpha < $t is seen as 0 for the bounding "
                                                   "box calculations such that you can remove nearly transparent "
                                                   "pixels. This will significantly slow down the execution", type=int)
+    parser.add_argument("--xoffset", "-xo", help="X-offset for a given anchor. This is the shift used in the mod "
+                                                 "code for the anchor.", type=float)
+    parser.add_argument("--yoffset", "-yo", help="Y-offset for a given anchor. This is the shift used in the mod "
+                                                 "code for the anchor.", type=float)
+    parser.add_argument("--xshift", "-xs", help="X-shift as determined by this program for the anchor object", type=float)
+    parser.add_argument("--yshift", "-ys", help="Y-shift as determined by this program for the anchor object", type=float)
 
     args = parser.parse_args()
 
@@ -129,7 +174,13 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    pack(args.dir, args.output, args.extension, args.scale, args.threshold)
+    print(args)
+
+    anchorShift = None
+    if any([args.xoffset, args.yoffset, args.xshift, args.yshift]):
+        anchorShift = AnchorShift(args.xoffset, args.yoffset, args.xshift, args.yshift)
+
+    pack(args.dir, args.output, args.extension, args.scale, args.threshold, anchorShift)
 
 
 if __name__ == "__main__":
