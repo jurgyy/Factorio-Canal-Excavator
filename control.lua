ac33208ec77275b342639d9c3571ed9baaeea1dd
Jurgen <jurgen.baas@outlook.com>
2024-05-02 23:09:53 +0200

    Cleaned up comments, unused imports and functions

--- control.lua
+++ control.lua
@@ -1,7 +1,3 @@
-local event = require("__flib__.event")
-local util = require("util")
-
-local ore_manager = require("oreManager")
 local dig_manager = require("digManager")
 
 local ghost_marker_built_event = require("events.ghostMarkerBuiltEvent")
@@ -13,10 +9,10 @@ local tile_mined_event = require("events.tileMinedEvent")
 -- Once dug, ground turned into "dug" tile
 -- Dug checks if it touches water
 -- If yes:
---   random delay [10, 60) ticks to turn into water
+--   random delay [15, 90) ticks to turn into water
 --   Notify surrounding dug tiles
 
-local function get_landfill_stone_amount()
+local function get_landfill_stone_cost()
   for _, ingredient in ipairs(game.recipe_prototypes["landfill"].ingredients) do
     if ingredient.name == "stone" then
       return ingredient.amount
@@ -24,7 +20,6 @@ local function get_landfill_stone_amount()
   end
   return 20
 end
-
 
 script.on_init(function()
   -- dug_to_water contains all dug tiles that have yet to be transformed into water. indexed by the tick they will transform
@@ -34,51 +29,19 @@ script.on_init(function()
   -- remaining_ore contains all tiles that were started, have since been removed. Indexed by [surface.index][x][y]
   global.remaining_ore = {}
 
-  global.ore_starting_amount = get_landfill_stone_amount()
+  global.ore_starting_amount = get_landfill_stone_cost()
 end)
-
--- script.on_migrations(function()
---     -- if global.dug == nil then
---     --   global.dug = {}
---     -- end
---     if game == nil then log("game is nill") else log("game is not nill") end
--- end)
 
 script.on_configuration_changed(function(configurationChangedData)
-  game.print("on_configuration_changed")
-  game.print("old version: " .. util.to_str(configurationChangedData.old_version))
-  game.print("new version: " .. util.to_str(configurationChangedData.new_version))
-  if configurationChangedData.mod_changes ~= nil then
-    game.print("mod changes:")
-    for _, k in ipairs(configurationChangedData.mod_changes) do
-      game.print("\t" .. k .. ": " .. util.to_str(configurationChangedData.mod_changes[k].old_version) .. " to " .. util.to_str(configurationChangedData.mod_changes[k].new_version))
-    end
-  end
-
-  game.print("mod startup settings changed: " .. util.to_str(configurationChangedData.mod_startup_settings_changed))
-  game.print("migration applied: " .. util.to_str(configurationChangedData.migration_applied))
-  
-  global.ore_starting_amount = get_landfill_stone_amount()
+  global.ore_starting_amount = get_landfill_stone_cost()
 end)
-
-
-local function local_tile_mined_event(event)
-  tile_mined_event(event)
-end
-
-local function debug_check_leftover_dug_event(event)
-  for i, dug in ipairs(global.dug_to_water) do
-      game.print(i .. " dug tile on " .. dug.position.x .. ", " .. dug.position.y)
-  end
-end
 
 script.on_event(defines.events.on_resource_depleted, dig_manager.resource_depleted_event)
 script.on_event(defines.events.on_player_built_tile, place_tile_event)
 script.on_event(defines.events.on_robot_built_tile, place_tile_event)
-script.on_event(defines.events.on_player_mined_tile, local_tile_mined_event)
-script.on_event(defines.events.on_robot_mined_tile, local_tile_mined_event)
+script.on_event(defines.events.on_player_mined_tile, tile_mined_event)
+script.on_event(defines.events.on_robot_mined_tile, tile_mined_event)
 script.on_event(defines.events.on_built_entity, ghost_marker_built_event)
 
 script.on_nth_tick(dig_manager.check_interval, dig_manager.periodic_check_dug_event)
-script.on_nth_tick(1, debug_check_leftover_dug_event)
 
