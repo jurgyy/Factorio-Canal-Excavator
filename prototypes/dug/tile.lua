830e4f1afbb2285227cf47d4f3e8165d957157bd
Jurgen <jurgen.baas@outlook.com>
2024-05-02 21:39:08 +0200

    Swapped tile graphics. Landfill is now the dug graphic while dirt is the undug graphic.

--- prototypes/dug/tile.lua
+++ prototypes/dug/tile.lua
@@ -4,21 +4,22 @@ return {
     order = "e[canal][dug]",
     
     collision_mask = {"ground-tile"},
-    variants = data.raw.tile["dirt-5"].variants,
-    transitions = data.raw.tile["dirt-5"].transitions,
-    transitions_between_transitions = data.raw.tile["dirt-5"].transitions_between_transitions,
+    variants = data.raw.tile["landfill"].variants,
+    transitions = data.raw.tile["landfill"].transitions,
+    transitions_between_transitions = data.raw.tile["landfill"].transitions_between_transitions,
     
-    walking_sound = data.raw.tile["dirt-5"].walking_sound,
+    walking_sound = data.raw.tile["landfill"].walking_sound,
     map_color={r=0.063, g=0.133, b=0.149},
     scorch_mark_color = {r = 0.312, g = 0.148, b = 0.147, a = 1.000},
-    pollution_absorption_per_second = data.raw.tile["dirt-5"].pollution_absorption_per_second,
-    vehicle_friction_modifier = data.raw.tile["dirt-5"].vehicle_friction_modifier,
+    pollution_absorption_per_second = data.raw.tile["landfill"].pollution_absorption_per_second,
+    vehicle_friction_modifier = data.raw.tile["landfill"].vehicle_friction_modifier,
     decorative_removal_probability = 1,
     
-    trigger_effect = data.raw.tile["dirt-5"].trigger_effect,
+    trigger_effect = data.raw.tile["landfill"].trigger_effect,
     
-    layer = 10,
+    layer = 99,
     autoplace = nil,
     walking_speed_modifier = 0.6,
-    tint = { r = 0.400, g = 0.400, b = 0.260,   a = 0.25 }
+    tint = { r = 0.500, g = 0.500, b = 0.03,   a = 0.2 },
+
   }
