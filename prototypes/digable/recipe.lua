5e0606f4e885827ee6ba2b919aeb8e7a282fd751
Jurgen <jurgen.baas@outlook.com>
2024-05-12 13:45:55 +0200

    Added recipe to digable tiles

--- prototypes/digable/recipe.lua
+++ prototypes/digable/recipe.lua
@@ -2,11 +2,14 @@ return
 {
     type = "recipe",
     name = "canex-rec-digable",
+    category = "crafting-with-fluid",
     normal =
     {
         energy_required = 0.5,
         ingredients =
         {
+            {type="item", name = "stone", amount = 5},
+            {type = "fluid", name = "water", amount = 25}
         },
         result = "canex-item-digable"
     },
@@ -15,6 +18,8 @@ return
         energy_required = 0.5,
         ingredients =
         {
+            {type = "item", name = "stone", amount = 10},
+            {type = "fluid", name = "water", amount = 50}
         },
         result = "canex-item-digable"
     }
