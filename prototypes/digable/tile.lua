ee4baa44293d6c948a09111fe48ddc1a65c40037
Jurgen <jurgen.baas@outlook.com>
2025-01-01 15:16:21 +0100

    chore: Removed absorption

--- prototypes/digable/tile.lua
+++ prototypes/digable/tile.lua
@@ -11,7 +11,6 @@ return
     walking_sound = data.raw.tile["dirt-5"].walking_sound,
     map_color={ r = 0.181, g = 0.104, b = 0.075},
     scorch_mark_color = {r = 1.000, g = 1.000, b = 1.000, a = 1.000},
-    pollution_absorption_per_second = 0,
     minable = {mining_time = 0.1, result = "canex-item-digable"},
     decorative_removal_probability = 1,
     walking_speed_modifier = 0.6,
