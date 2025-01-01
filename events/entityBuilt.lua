1e082f0f9ce71056408104950f1c5e0282516160
Jurgen <jurgen.baas@outlook.com>
2025-01-01 14:42:19 +0100

    feat: When ghost excavatable tiles aren't able to be placed for any reason the place-ghost events should now be removed from the player's undo-stack as if nothing happened.

--- events/entityBuilt.lua
+++ events/entityBuilt.lua
@@ -26,6 +26,8 @@ local function remove_ghosts_tiles_at_entity(surface, entity)
     end
 end
 
+---@param surface LuaSurface
+---@param entity LuaEntity
 local function is_on_ghost_landfill(surface, entity)
   local entities = surface.find_entities_filtered{
     position = entity.position,
@@ -39,25 +41,44 @@ local function is_on_ghost_landfill(surface, entity)
   return false
 end
 
+---Clear the undo stack of the latest place canex-tile-digable actions.
+---Works only for ghost tiles. The tile version is in placeTileEvent.lua
+---@param player LuaPlayer
+local function clear_undo_stack(player)
+  local undo_redo_stack = player.undo_redo_stack
+  local undo_items = undo_redo_stack.get_undo_item(1)
+
+  -- Last undo action of first undo item should be placing digable tile
+  if undo_items[#undo_items].new_tile ~= "canex-tile-digable" then
+    return
+  end
+  undo_redo_stack.remove_undo_item(1)
+end
+
+---@param event EventData.on_built_entity
 local function handle_ghost_digable_tile(event)
   local entity = event.entity
   local surface = entity.surface
   local valid = util.surface_is_valid(surface)
+  local undone = false
 
   if not valid then
     util.show_error({"story.canex-invalid-surface"}, surface, {entity.position.x + 0.65, entity.position.y + 0.40})
     remove_ghosts_tiles_at_entity(surface, entity)
+    undone = true
   else
     if util.is_position_landfilled(surface, entity.position) then
       util.show_error({"story.canex-not-on-landfill"}, surface, {entity.position.x + 0.65, entity.position.y + 0.40})
       entity.destroy()
+      undone = true
     elseif is_on_ghost_landfill(surface, entity) then
       util.show_error({"story.canex-not-in-water"}, surface, {entity.position.x + 0.65, entity.position.y + 0.40})
       remove_ghosts_tiles_at_entity(surface, entity)
+      undone = true
     elseif dig_manager.is_dug(entity.surface, entity.position) then
       util.show_error({"story.canex-already-dug"}, surface, {entity.position.x + 0.65, entity.position.y + 0.40})
       entity.destroy()
-
+      undone = true
     elseif event.player_index and settings.get_player_settings(event.player_index)["auto-deconstruct"].value then
       -- Mark entities on ghost tile for deconstruction
       local non_excavators = ore_manager.get_colliding_entities(entity.surface, entity.position)
@@ -66,6 +87,13 @@ local function handle_ghost_digable_tile(event)
         or collidingEntity.ghost_prototype.name ~= digableTileName then
           collidingEntity.order_deconstruction(game.get_player(event.player_index).force)
         end
+      end
+    end
+
+    if undone and event.player_index then
+      player = game.players[event.player_index]
+      if player then
+        clear_undo_stack(player)
       end
     end
   end
@@ -79,6 +107,9 @@ local function entity_built_event(event)
       entity.rotatable = false
 
     elseif is_digable(entity) then
+      ---@cast event EventData.on_built_entity
+      --Robots can't build ghosts
+      --TODO Scripts can build ghosts
       handle_ghost_digable_tile(event)
     end
   end
