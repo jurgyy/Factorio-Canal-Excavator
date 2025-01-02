f8e66c9435150b8373eb302fa0da0de13e5f0944
Jurgen <jurgen.baas@outlook.com>
2025-01-02 17:12:52 +0100

    Introduced planetsManager

--- events/tileMinedEvent.lua
+++ events/tileMinedEvent.lua
@@ -1,10 +1,13 @@
 local ore_manager = require("oreManager")
 local dig_manager = require("digManager")
+local planets_manager = require("planetsManager")
 local digableTileName = require("getTileNames").digable
 
 
 local function tile_mined_event(event)
   local surface = game.surfaces[event.surface_index]
+  local planet_config = planets_manager.get_planet_config(surface)
+
   for _, tile in ipairs(event.tiles) do
     if tile.old_tile.name == digableTileName then
       local ores = surface.find_entities_filtered{
@@ -17,7 +20,7 @@ local function tile_mined_event(event)
       for _, ore in pairs(ores) do
         -- TODO all entities are at the same pos so in the case of multiple entities they overwrite eachother
         -- Does that ever happen? Will trigger print in insert function.
-        if ore.amount < storage.ore_starting_amount then
+        if ore.amount < planet_config.oreStartingAmount then
           ore_manager.insert_stored_ore_amount(surface, tile.position, ore.amount)
         end
 
