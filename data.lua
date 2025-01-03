296b3516f86502d90494fadebdc79b5887426c87
Jurgen <jurgen.baas@outlook.com>
2025-01-03 20:04:31 +0100

    Register the vanilla planets

--- data.lua
+++ data.lua
@@ -1,4 +1,12 @@
+require("prototypes.globalFunctions")
+
 require("prototypes.excavator.data")
 require("prototypes.digable.data")
 require("prototypes.dug.data")
 require("prototypes.technology")
+
+local planetConfigs = require("planetConfig")
+
+for name, config in pairs(planetConfigs) do
+    canex_register_planet_config(name, config)
+end
