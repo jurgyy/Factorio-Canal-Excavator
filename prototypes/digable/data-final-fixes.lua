cf6ad4f69a088920d976989987f9a9cea0783e27
Jurgen <jurgen.baas@outlook.com>
2025-01-04 22:46:19 +0100

    Resources in space age have their planet in the icon background

--- prototypes/digable/data-final-fixes.lua
+++ prototypes/digable/data-final-fixes.lua
@@ -12,5 +12,25 @@ for name, config in pairs(configs) do
     resource.mining_visualisation_tint = config.tint
     resource.stages.sheet.tint = config.tint
     resource.icons[1].tint = config.tint
+
+    if mods["space-age"] then
+        local planet = data.raw["planet"][name]
+        if planet then
+            local planet_localisation = planet.localised_name or ("space-location-name." .. name)
+            resource.localised_name = {"entity-name.canex-rsc-digable-planet", {planet_localisation}}
+
+            resource.icons[2] = resource.icons[1]
+            if planet.icons then
+                resource.icons[1] = planet.icons[1]
+            else
+                resource.icons[1] = {
+                    icon = planet.icon,
+                }
+            end
+            resource.icons[1].scale = 0.5
+            resource.icons[1].shift = {x = -16, y = -16}
+        end
+    end
+
     data:extend({resource})
 end
