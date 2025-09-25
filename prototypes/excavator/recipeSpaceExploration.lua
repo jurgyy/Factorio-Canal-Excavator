3187fe27dc5edb3978fb309dd29921fd844f612f
Jurgen <jurgen.baas@outlook.com>
2025-09-25 22:30:36 +0200

    fix: Fixed crash on startup when Space Exploration is installed.

--- prototypes/excavator/recipeSpaceExploration.lua
+++ prototypes/excavator/recipeSpaceExploration.lua
@@ -1,10 +1,10 @@
 local recipe = require("__canal-excavator__/prototypes/excavator/recipe")
 
 recipe.ingredients = {
-    {"steel-plate", 50},
-    {"concrete", 25},
-    {"iron-gear-wheel", 5},
-    {"electric-engine-unit", 5}
+    {type = "item", name = "steel-plate", amount = 50},
+    {type = "item", name = "concrete", amount = 25},
+    {type = "item", name = "iron-gear-wheel", amount = 5},
+    {type = "item", name = "electric-engine-unit", amount = 5}
 }
 
 return recipe
