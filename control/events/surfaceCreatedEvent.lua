jurgyy/Factorio-Canal-Excavator

commit 15778d146e237de70e8ac3f08a34821e51302729

---@param config CanexSurfaceConfig feat: Removed default configuration. Now each surface is required to configure a CanexSurfaceConfig or CanexSurfaceTemplate.

control/surfacesManager.lua
@@ -3,19 +3,6 @@ local surface_config_helper = require("global.surfaceConfigHelper")
 local surfaceConfigs = surface_config_helper.get_all_surface_config()
 local resource_names = {}
 
----@type CanexSurfaceConfig
-local defaultConfig = nil
-for name, config in pairs(surfaceConfigs) do
-  resource_names[name] = "canex-rsc-digable-" .. config.surfaceName
-  if config.isDefault then
-    if defaultConfig ~= nil then
-      error(name .. " is already configured as default")
-    end
-    defaultConfig = config
-  end
-end
-if not defaultConfig then error("No default surface configured") end
-
 surfacesManager = {}
 
 surfacesManager.resource_names = resource_names

global/surfaceConfigHelper.lua
@@ -1,10 +1,3 @@
----@class CanexConfigBase Base class for both SurfaceConfig as well as SurfaceTemplates
----@field localisation LocalisedString? Localised name to use for the resource: "Excavatable resource {localisation}". Expected to be lower case except for proper nouns.
----@field mineResult string Mine result item
----@field oreStartingAmount integer Amount of ore that should be placed when placing a excavatable tile
----@field tint Color Tint for the dust, rocks and resource
-
---------------------------------
 --- Surface Config
 ---@class CanexSurfaceConfigModData : data.ModData
 ---@field data_type "canex-surface-config"
@@ -14,9 +7,15 @@
 ---@field data_type "canex-surface-config"
 ---@field data CanexSurfaceConfig
 
+---@class CanexConfigBase Base class for both SurfaceConfig as well as SurfaceTemplates
+---@field localisation LocalisedString? Localised name to use for the resource: "Excavatable resource {localisation}". Expected to be lower case except for proper nouns.
+---@field mineResult string Mine result item
+---@field oreStartingAmount integer Amount of ore that should be placed when placing a excavatable tile
+---@field tint Color Tint for the dust, rocks and resource
+
 ---@class CanexSurfaceConfig : CanexConfigBase
 ---@field surfaceName string
----@field isDefault boolean? Fallback config for surfaces without a planet. Only one config can be the default TODO still required?
+-- Also contains fields of CanexConfigBase (see above)
 
 --------------------------------
 --- Surface Template
@@ -31,6 +30,7 @@
 ---@class CanexSurfaceTemplate : CanexConfigBase
 ---@field icon table?
 ---@field name string name of the parent object. Will be set in Canex data-final-fixes.
+-- Also contains fields of CanexConfigBase (see above)
 
 --------------------------------
 --- Remote for surface created event handler

prototypes/surfaceConfig.lua
@@ -17,8 +17,7 @@ data:extend{
     localisation = {"space-location-name.nauvis"},
     mineResult = "stone",
     oreStartingAmount = 10,
-    tint = {r = 102, g = 78, b = 6},
-    isDefault = true
+    tint = {r = 102, g = 78, b = 6}
   })
 }
 

commit dfb49ce496c7e7f2dd64b9c717ddc5d984484b93

fixed bug introduced by 15778d146e237de70e8ac3f08a34821e51302729 where non-template surfaces weren't loading their resource names anymore.

control/events/surfaceCreatedEvent.lua
@@ -6,6 +6,9 @@ local surface_manager = require("control.surfacesManager")
 ---@param event EventData.on_surface_created
 local function surface_created_event(event)
   local surface = game.surfaces[event.surface_index]
+  if surface_manager.get_surface_config(surface) then
+    return
+  end
   local surface_template = nil
 
   for _, sc_remote in pairs(remotes_manager.surface_created_remotes) do

control/surfacesManager.lua
@@ -3,15 +3,19 @@ local surface_config_helper = require("global.surfaceConfigHelper")
 local surfaceConfigs = surface_config_helper.get_all_surface_config()
 local resource_names = {}
 
+for name, config in pairs(surfaceConfigs) do
+  resource_names[name] = "canex-rsc-digable-" .. config.surfaceName
+end
+
 surfacesManager = {}
 
 surfacesManager.resource_names = resource_names
 
 surfacesManager.load_stored_config = function()
   ---@cast surfaceConfigs +table<string, CanexSurfaceTemplate>
-  for surfaceName, surfaceConfig in pairs(storage.runtime_surface_config or {}) do
-    surfaceConfigs[surfaceName] = surfaceConfig
-    resource_names[surfaceName] = "canex-rsc-digable-" .. surfaceConfig.name
+  for surfaceName, surfaceTemplate in pairs(storage.runtime_surface_config or {}) do
+    surfaceConfigs[surfaceName] = surfaceTemplate
+    resource_names[surfaceName] = "canex-rsc-digable-" .. surfaceTemplate.name
   end
 end
 