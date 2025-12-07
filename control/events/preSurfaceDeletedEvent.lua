a66b857cfdf9e8b3d63cc91340f4acb16e5fc2c9
Jurgen <jurgen.baas@outlook.com>
2025-12-07 20:08:33 +0100

    fix: Fixed possible crash when a surface is deleted

--- control/events/preSurfaceDeletedEvent.lua
+++ control/events/preSurfaceDeletedEvent.lua
@@ -1,7 +1,7 @@
 ---@param event EventData.on_pre_surface_deleted
 local function pre_surface_deleted_event(event)
     local surface = game.get_surface(event.surface_index)
-    if surface then
+    if surface and storage.runtime_surface_config then
         storage.runtime_surface_config[surface.name] = nil
     end
 end
