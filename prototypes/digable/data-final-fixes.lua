116100d6b2341de18c499b57ac35d3e7d9185026
Jurgen <jurgen.baas@outlook.com>
2025-12-12 17:02:48 +0100

    feat: Added support for custom multi-layer icons for the Factoriopedia page of the excavatable resources.

--- prototypes/digable/data-final-fixes.lua
+++ prototypes/digable/data-final-fixes.lua
@@ -3,81 +3,82 @@ local surface_configs = surface_config_helper.get_all_surface_config()
 
 local resourceTemplate = require("prototypes.digable.resourceTemplate")
 
-for _, surface_config in pairs(surface_configs) do
-  log("Adding Canex config for surface " .. surface_config.surfaceName)
+---@param resource data.ResourceEntityPrototype
+---@param config CanexSurfaceConfig|CanexSurfaceTemplate
+local function handle_icons(resource, config)
+  local icon_data = config.icon_data
+  local icon_source_type = icon_data and icon_data.icon_source_type or (mods["space-age"] and "planet")
+  local icon_source_name = icon_data and icon_data.icon_source_name or config.surfaceName
+  local filename =  icon_data and icon_data.filename
+  local icons = icon_data and icon_data.icons
+  local icon_size = icon_data and icon_data.icon_size
 
-  local resource = table.deepcopy(resourceTemplate)
-  resource.name = resource.name .. surface_config.surfaceName
-  resource.minable.results[1].name = surface_config.mineResult
-  resource.map_color = surface_config.tint
-  resource.mining_visualisation_tint = surface_config.tint
-  resource.stages.sheet.tint = surface_config.tint
-  resource.icons[1].tint = surface_config.tint
-
-  if mods["space-age"] then
-    local planet = data.raw["planet"][surface_config.surfaceName]
-    if planet then
-      resource.icons[2] = resource.icons[1]
-      if planet.icons then
-        resource.icons[1] = planet.icons[1]
-      else
-        resource.icons[1] = {
-          icon = planet.icon,
-        }
+  local icon_layers = {}
+  if icon_source_type and icon_source_name or filename or icons then
+    if filename then
+      icon_layers[1] = {
+        icon = filename,
+      }
+    elseif icons then
+      icon_layers = icons
+    else
+      if not icon_source_type or not icon_source_name or not data.raw[icon_source_type] then
+        error("Can't find icon for " .. (config.surfaceName or "template"))
       end
-      resource.icons[1].scale = 0.5
-      resource.icons[1].shift = {x = -16, y = -16}
+      local icon_source = data.raw[icon_source_type][icon_source_name]
+      if icon_source then
+        if icon_source.icons then
+          icon_layers = icon_source.icons
+        else
+          icon_layers[1] = {
+            icon = icon_source.icon,
+          }
+        end
+      end
+    end
+
+    resource.icons[#icon_layers + 1] = resource.icons[1]
+    ---@diagnostic disable-next-line: param-type-mismatch
+    for i, icon in ipairs(icon_layers) do
+      icon_size = icon_size or icon.icon_size or 64
+      resource.icons[i] = icon
+      resource.icons[i].icon_size = icon_size
+      resource.icons[i].scale = 0.5 * (64 / icon_size)
+      resource.icons[i].shift = {x = -16, y = -16}
+      resource.icons[i].tint = icon.tint
     end
   end
+end
 
-  if surface_config.localisation then
-    resource.localised_name =  {"entity-name.canex-rsc-digable-surface", {"entity-description.canex-on", surface_config.localisation}}
-    resource.localised_description = {"entity-description.canex-rsc-digable-surface", {"entity-description.canex-on", surface_config.localisation}}
+---@param config CanexSurfaceConfig|CanexSurfaceTemplate
+---@return data.ResourceEntityPrototype
+local function create_resource_copy(config)
+  local resource = table.deepcopy(resourceTemplate)
+  resource.name = resource.name .. (config.surfaceName or config.name)
+  resource.minable.results[1].name = config.mineResult
+  resource.map_color = config.tint
+  resource.mining_visualisation_tint = config.tint
+  resource.stages.sheet.tint = config.tint
+  resource.icons[1].tint = config.tint
+
+  handle_icons(resource, config)
+  if config.localisation then
+    resource.localised_name = {"entity-name.canex-rsc-digable-surface", {"entity-description.canex-in", config.localisation}}
+    resource.localised_description = {"entity-description.canex-rsc-digable-surface", {"entity-description.canex-in", config.localisation}}
   end
+  return resource
+end
 
-  data:extend({resource})
+for _, surface_config in pairs(surface_configs) do
+  log("Adding Canex config for surface " .. surface_config.surfaceName)
+  data:extend({create_resource_copy(surface_config)})
 end
 
 for name, mod_data in pairs(data.raw["mod-data"]) do
   if mod_data.data_type == surface_config_helper.surface_template_data_type then
     ---@cast mod_data CanexSurfaceTemplateModData
-
     local template_config = mod_data.data
-    -- config.name = mod_data.name
     data.raw["mod-data"][name].data.name = name
-    local resource = table.deepcopy(resourceTemplate)
-    resource.name = resource.name .. mod_data.name
-    if template_config.localisation then
-      resource.localised_name = {"entity-name.canex-rsc-digable-surface", {"entity-description.canex-in", template_config.localisation}}
-      resource.localised_description = {"entity-description.canex-rsc-digable-surface", {"entity-description.canex-in", template_config.localisation}}
-    end
-    resource.minable.results[1].name = template_config.mineResult
-    resource.map_color = template_config.tint
-    resource.mining_visualisation_tint = template_config.tint
-    resource.stages.sheet.tint = template_config.tint
-    resource.icons[1].tint = template_config.tint
-
-    local icon = template_config.icon
-    if icon then
-      if (icon.type) then
-        local prototype = data.raw[icon.type][icon.name]
-        resource.icons[2] = resource.icons[1]
-        if prototype.icons then
-          resource.icons[1] = prototype.icons[1]
-        else
-          resource.icons[1] = {
-            icon = prototype.icon,
-          }
-        end
-        resource.icons[1].shift = {x = -16, y = -16}
-      else
-        resource.icons[2] = resource.icons[1]
-        resource.icons[2].scale = 0.5 / icon.scale
-        resource.icons[1] = icon
-        resource.icons[1].shift = {x = -16 / icon.scale, y = -16 / icon.scale}
-      end
-      --resource.icons[1].scale = 0.5
-    end
-    data:extend({resource})
+    data:extend({create_resource_copy(template_config)})
   end
 end
